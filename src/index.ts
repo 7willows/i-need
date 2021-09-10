acf483884322ca593e472a6ca8e52b02a937927c
Szymon <sw@7willows.com>
2021-09-10 12:00:36 +0200

    basic assertion classes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-export function nonEmptyStrings(params:object) {
-
+export class AssertionError extends Error {
+    constructor(msg:string) {
+        super(msg);
+        this.name = AssertionError.name;
+    }
 }
+
+export const nonEmptyStrings = assertAllFactory((propValue: unknown, propName: string) => {
+    if (typeof propValue !== 'string') {
+        throw new AssertionError(`${propName} must be non empty string`);
+    }
+});
+
+export const stringsOrUndefined = assertAllFactory((propValue: unknown, propName: string) => {
+    if (typeof propValue !== 'string' && propValue !== undefined) {
+        throw new AssertionError(`If ${propName} is specified it must be a string`);
+    }
+});
+
+export const arrays = assertAllFactory((propValue: unknown, propName: string, _all: InputProperties, msg?: string) => {
+    if (!Array.isArray(propValue)) {
+        throw new AssertionError(msg ?? `${propName} must be an array`);
+    }
+});
+
+interface InputProperties {
+    [key: string]: unknown;
+}
+
+function assertAllFactory(func: (prop: unknown, propName: string, allProps: InputProperties, msg?: string) => void) {
+    return function(properties: InputProperties, msg?: string) {
+        Object.keys(properties).forEach(propName => func(properties[propName], propName, properties, msg));
+    }
+}
+
+
--- test/tests.ts
+++ test/tests.ts
@@ -1,6 +1,35 @@
 import test from 'ava';
 import * as iNeed from '../src/';
 
-test('function is defined', t => {
+test('nonEmptyStrings: function is defined', t => {
     t.notThrows(() => iNeed.nonEmptyStrings({}));
+});
+
+test('nonEmptyStrings: without strings it fails', t => {
+    const a = 2;
+    t.throws(() => iNeed.nonEmptyStrings({ a }), {
+        instanceOf: iNeed.AssertionError
+    });
+});
+
+test('stringsOrUndefined: throws on not strings', t => {
+    const a = 2;
+    t.throws(() => iNeed.stringsOrUndefined({ a }), {
+        name: 'AssertionError'
+    });
+});
+
+test('stringsOrUndefined: does not throw on undefined', t => {
+    const a = undefined;
+    t.notThrows(() => iNeed.stringsOrUndefined({ a }));
+});
+
+test('arrays: throws on not arrays', t => {
+    const a = { length: 0 };
+    t.throws(() => iNeed.arrays({ a }));
+});
+
+test('arrays: does not throw on arrays', t => {
+    const a: unknown[] = [];
+    t.notThrows(() => iNeed.arrays({ a }));
 });
