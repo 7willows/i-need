9c0b5029a846d64b605666c7d8fc300e85db090d
Tomasz Kubiak <tomasz.kubiak@7willows.com>
2021-09-23 06:36:41 +0200

    feat numbers

--- src/index.ts
+++ src/index.ts
@@ -47,41 +47,78 @@ export const specificValues = assertAllFactory((propValue, propName, _allProps, 
     }
 });
 
-export const numbers = assertAllFactory((propValue: unknown, propName: string, _allProps, opts:unknown) => {
+interface NumbersOptions {
+    greaterThan: number|undefined;
+    greaterThanOrEqual: number|undefined;
+    lessThan: number|undefined;
+    lessThanOrEqual: number|undefined;
+}
+
+function getMsg(
+    propName: string,
+    { greaterThan, greaterThanOrEqual, lessThan, lessThanOrEqual }: NumbersOptions
+) {
     const msgs = [];
-    let res = true;
-    opts = (typeof opts === 'object' ? opts : {});
+
+    if (typeof greaterThan === 'number') {
+        msgs.push(`greater than ${greaterThan}`)
+    } 
+
+    if (typeof greaterThanOrEqual === 'number') {
+        msgs.push(`greater than or equal ${greaterThanOrEqual}`)
+    } 
+
+    if (typeof lessThan === 'number') {
+        msgs.push(`less than ${lessThan}`)
+    } 
+
+    if (typeof lessThanOrEqual === 'number') {
+        msgs.push(`less than or equal ${greaterThanOrEqual}`)
+    } 
+
+    return `${propName} must be a number ${msgs.join(' and ')}`
+}
+
+
+export const numbers = assertAllFactory((propValue: unknown, propName: string, _allProps, opts: unknown) => {
+    const config: NumbersOptions = (opts instanceof Object ? opts : {} as any);
+
     if (typeof propValue !== 'number') {
-        res = false;
+        return getMsg(propName, config);
+    } 
+
+    if (config.greaterThan !== undefined && typeof config.greaterThan !=='number') {
+        throw new Error(`can't compare to non-number - 'greaterThan' must be number or undefined`)
     }
-    if (typeof opts?.greater === 'number') {
-        msgs.push(`greater than ${opts.greater}`)
-        if (propValue <= opts.greater){
-            res = false;
-        }
+    
+    if (typeof config.greaterThan === 'number' && config.greaterThan >= propValue) {
+        return getMsg(propName, config);
     }
-    if (typeof opts?.greaterOrEqual === 'number') {
-        msgs.push(`greater than or equal ${opts.greaterOrEqual}`)
-        if (propValue < opts.greaterOrEqual){
-            res = false;
-        }
+    
+    if (config.greaterThanOrEqual !== undefined && typeof config.greaterThanOrEqual !=='number') {
+        throw new Error(`can't compare to non-number - 'greaterThanOrEqual' must be number or undefined`)
     }
-    if (typeof opts?.less === 'number') {
-        msgs.push(`less than ${opts.less}`)
-        if (propValue >= opts.less){
-            res = false;
-        }
+    
+    if (typeof config.greaterThanOrEqual === 'number' && config.greaterThanOrEqual > propValue) {
+        return getMsg(propName, config);
     }
-    if (typeof opts?.lessOrEqual === 'number') {
-        msgs.push(`less than or equal ${opts.greaterOrEqual}`)
-        if (propValue > opts.greaterOrEqual){
-            res = false;
-        }
+    
+    if (config.lessThan !== undefined && typeof config.lessThan !=='number') {
+        throw new Error(`can't compare to non-number - 'lessThan' must be number or undefined`)
     }
-    if (!res) {
-        return `${propName} must be a number ${msgs.join(' and ')}`
+    
+    if (typeof config.lessThan === 'number' && config.lessThan <= propValue) {
+        return getMsg(propName, config);
     }
-});
+    
+    if (config.lessThanOrEqual !== undefined && typeof config.lessThanOrEqual !=='number') {
+        throw new Error(`can't compare to non-number - 'lessThanOrEqual' must be number or undefined`)
+    }
+    
+    if (typeof config.lessThanOrEqual === 'number' && config.lessThanOrEqual < propValue) {
+        return getMsg(propName, config);
+    }
+    });
 
 interface InputProperties {
     [key: string]: unknown;
